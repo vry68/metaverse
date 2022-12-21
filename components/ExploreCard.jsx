d51a994f4c2bc1b310d87d5390832b4a66d0eae9
vryl68 <dharylcocos246@gmail.com>
2022-12-21 21:16:13 +0800

    a

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -15,7 +15,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   >
     <img
       src={imgUrl}
-      alt='title'
+      alt="title"
       className="absolute w-full h-full object-cover rounded-[24px]"
     />
     {active !== id ? (
@@ -29,7 +29,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
         >
           <img
             src='/headset.svg'
-            alt='headset'
+            alt="headset"
             className="w-1/2 h-1/2 object contain"
           />
         </div>
