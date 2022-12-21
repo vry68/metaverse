ff8e70536687ffb27c070f01f62222b8c8b6df1c
vryl68 <dharylcocos246@gmail.com>
2022-12-21 22:12:07 +0800

    aaa

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => (
         className="w-[24px] h-[24px] object-contain"
       />
       <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
-        METAVERSUS
+        i love you :3
       </h2>
       <img
         src="/menu.svg"
